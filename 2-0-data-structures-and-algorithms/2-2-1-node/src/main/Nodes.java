64e26af55f5423357323eaeaf943cd3835bf91d8
myroslavFF <gardenov6@gmail.com>
2022-09-09 18:40:01 +0300

    Initial commit

---
+++ 2-0-data-structures-and-algorithms/2-2-1-node/src/main/Nodes.java
@@ -0,0 +1,106 @@
+package com.bobobode.cs;
+
+
+/**
+ * A class that consists of static methods only and provides util methods for {@link Node}.
+ *
+ */
+public class Nodes {
+    private Nodes() {
+    }
+
+    /**
+     * Creates a new instance of {@link Node} that holds provided element
+     *
+     * @param element any element of type T
+     * @param <T>     generic type
+     * @return a new instance of {@link Node}
+     */
+    public static <T> Node<T> create(T element) {
+        return new Node<>(element);// todo:
+    }
+
+    /**
+     * Create a connection between first and second nodes, so object first stores a reference to the second.
+     *
+     * @param first  any {@link Node} object
+     * @param second any {@link Node} object
+     * @param <T>    a genetic type
+     */
+    public static <T> void link(Node<T> first, Node<T> second) {
+       first.next = second;
+    }
+
+    /**
+     * Creates two new {@link Node} objects using provided firstElement and secondElement, and create a connection
+     * between those two elements so the first node will hold a reference to a second one.
+     *
+     * @param firstElement  any element of type T
+     * @param secondElement any element of type T
+     * @param <T>           a genetic type
+     * @return a reference to a first node created based on firstElement
+     */
+    public static <T> Node<T> pairOf(T firstElement, T secondElement) {
+        Node<T> first = new Node<>(firstElement);
+        first.next = new Node<>(secondElement);
+        return first;
+    }
+
+
+    /**
+     * Creates two new {@link Node} objects using provided firstElement and secondElement, and creates connections
+     * between those nodes so the first node will hold a reference to a second one, and a second node will hold
+     * a reference to the first one.
+     *
+     * @param firstElement  any element of type T
+     * @param secondElement any element of type T
+     * @param <T>           generic type T
+     * @return a reference to the first node
+     */
+    public static <T> Node<T> closedPairOf(T firstElement, T secondElement) {
+        Node<T> first = new Node<>(firstElement);
+        Node<T> second = new Node<>(secondElement);
+        first.next = second;
+        second.next = first;
+        return first;
+    }
+
+    /**
+     * Creates a linked chain of {@link Node} objects based on provided elements. Creates a connection between those
+     * nodes so each node will hold a reference to the next one in the chain. HINT: it's basically a linked list.
+     *
+     * @param elements a array of elements of type T
+     * @param <T>      generic type T
+     * @return a reference to the first element of the chain
+     */
+    public static <T> Node<T> chainOf(T... elements) {
+        Node<T> firs = new Node<>(elements[0]);
+        Node<T> current = firs;
+        for (int i = 1; i < elements.length; i++) {
+            current.next = new Node<>(elements[i]);
+            current = current.next;
+        }
+        return firs;
+    }
+
+    /**
+     * Creates a linked circle of {@link Node} objects based on provided elements. Creates a connection between those
+     * nodes so each node will hold a reference to the next one in the chain, and the last one will hold a reference to
+     * the first one.
+     *
+     * @param elements a array of elements of type T
+     * @param <T>      generic type T
+     * @return a reference to the first element of the chain
+     */
+    public static <T> Node<T> circleOf(T... elements) {
+        Node<T> firs = new Node<>(elements[0]);
+        Node<T> current = firs;
+        for (int i = 1; i < elements.length; i++) {
+            current.next = new Node<>(elements[i]);
+            current = current.next;
+        }
+        current.next = firs;
+        return firs;
+    }
+
+}
